dca269b5318234eb2705b20689a70ae95d9c9dc2
VAISHNAVISTUDENT <vaishnavilanjewar503@gmail.com>
2024-10-23 12:58:59 +0530

    Initial commit2

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
-import FrontPage from '../components/Front_Page/FrontPage';
-import FeatureCompo from '../components/Feature_component/FeatureCompo';
-import ReviewPage from '../components/Review_page/ReviewPage';
-import Carousel from '../components/Review_page/carousel';
+
 import LandingPage from '../components/LandingPage/LandingPage'
 
 
